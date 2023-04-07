eb1f4950c0def0bff4d1864c20e756e935a27b87
Saria Elchaar <s.elchaar@hotmail.com>
2023-04-07 15:13:42 +1000

    Added content in renderLicensebadge function and renderLicenseLink function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# asdf
+# readme gen
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,51 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// If there is no license, it will return an empty string return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  switch (license.toLowerCase()) {
+    case "MIT":
+      return "https://img.shields.io/badge/License-MIT-yellow.svg";
+    case "ISC":
+      return "https://img.shields.io/badge/License-ISC-blue.svg";
+    case "MS-PL":
+      return "https://img.shields.io/badge/License-MS--PL-brightgreen.svg";
+    case "LGPL":
+      return "https://img.shields.io/badge/License-LGPL_v3-blue.svg";
+    default:
+      return "";
+  }
+}
+
+// This code first generates the badge URl using the renderLicenseBadge function, then check if the URL is empty, it's found then the license link will be generated using the renderLicenseLink
+const badge = renderLicenseBadge(data.license);
+
+if (badge) {
+  markdown += `[![License](${badge})](${renderLicenseLink(data.license)})\n\n`;
+}
+
+// Function that returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+
+  switch (license.toLowerCase()) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "ISC":
+      return "https://opensource.org/licenses/ISC";
+    case "MS-PL":
+      return "https://opensource.org/licenses/MS-PL";
+    case "LGPL":
+      return "https://opensource.org/licenses/LGPL-3.0";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
